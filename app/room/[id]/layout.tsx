0a5509afc42b4f7582aa298c361dc3c9693f64cf
szamanr <szamanr@protonmail.com>
2024-02-06 12:22:34 +0100

    create room page

---
+++ app/room/[id]/layout.tsx
@@ -0,0 +1,17 @@
+import { ParentComponentProps } from "@/utils/componentTypes";
+import Link from "next/link";
+import Icon from "@/components/Icon";
+import { NavBar } from "@/components/NavBar";
+
+export default function Layout({ children }: ParentComponentProps) {
+  return (
+    <div>
+      <NavBar>
+        <Link href="/rooms">
+          <Icon name="chevron_left" />
+        </Link>
+      </NavBar>
+      {children}
+    </div>
+  );
+}
