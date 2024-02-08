533594372ba1edcd660417bda7777c19984c4d29
szamanr <szamanr@protonmail.com>
2024-02-08 09:45:07 +0100

    query data based on search

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -4,9 +4,14 @@ import { Room } from "@/app/rooms/types";
 import Image from "next/image";
 import { For } from "@/components/controlFlow/For/For";
 import { ErrorMessage } from "@/components/return/ErrorMessage";
+import { Searchbox } from "@/app/rooms/Searchbox";
+import { SearchParams } from "@/utils/componentTypes";
 
-const Rooms = async () => {
-  const { data, error } = await supabaseServerClient().from("room").select();
+const Rooms = async ({ searchParams }: SearchParams) => {
+  const search = searchParams.name;
+  let queryRooms = supabaseServerClient().from("room").select();
+  if (search) queryRooms = queryRooms.ilike("name", `%${search}%`);
+  const { data, error } = await queryRooms;
   const rooms: Room[] = data ?? [];
 
   if (error) {
@@ -15,7 +20,10 @@ const Rooms = async () => {
   }
 
   return (
-    <div>
+    <div className="w-5/6 flex flex-col items-center gap-4">
+      <div className="w-1/2">
+        <Searchbox className="w-full p-2 rounded border" param="name" />
+      </div>
       <ul className="grid grid-cols-3 gap-6">
         <For each={rooms}>
           {({ id, name, cover_photo }) => (
