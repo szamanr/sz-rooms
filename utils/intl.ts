e18631a3db38abd0a6707d1c419438cc3ceeb798
szamanr <szamanr@protonmail.com>
2024-02-12 10:57:06 +0100

    make it possible to pass variables to intl util

--- utils/intl.ts
+++ utils/intl.ts
@@ -1,7 +1,19 @@
 const Languages = ["en"] as const;
 type Language = (typeof Languages)[number];
 
+const translate = (s: string) => s;
+
 // @todo add intl library
-export const $t = (string: string, lang: Language = "en"): string => {
-  return string;
+export const $t = (
+  string: string,
+  variables?: Record<string, string | number>,
+): string => {
+  if (!variables) return translate(string);
+
+  let parsedString = string;
+  for (const [key, value] of Object.entries(variables)) {
+    parsedString = parsedString.replaceAll(`{${key}}`, `${value}`);
+  }
+
+  return parsedString;
 };
