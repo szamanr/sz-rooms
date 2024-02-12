dac0c5e05d597f89e4467b5722e511d664b0aff0
szamanr <szamanr@protonmail.com>
2024-02-12 10:57:45 +0100

    improve fallback styling

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -24,7 +24,7 @@ const Rooms = async ({ searchParams }: SearchParams) => {
     return <ErrorMessage />;
   }
 
-  const fallback = <div>{$t("no rooms found")}</div>;
+  const fallback = <div className="text-gray-500">{$t("no rooms found")}</div>;
 
   return (
     <div className="w-5/6 flex flex-col items-center gap-4">
