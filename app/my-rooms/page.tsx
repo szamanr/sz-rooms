eca16ca9ba07f69a9861feacb708ad9825e7558c
szamanr <szamanr@protonmail.com>
2024-02-29 10:36:16 +0000

    allow adding new room from my rooms page

--- app/my-rooms/page.tsx
+++ app/my-rooms/page.tsx
@@ -4,13 +4,17 @@ import { Searchbox } from "@/app/rooms/Searchbox";
 import { SearchParams } from "@/utils/componentTypes";
 import { $t } from "@/utils/intl";
 import { RoomFilters } from "@/app/rooms/RoomFilters";
-import invariant from "tiny-invariant";
 import { RoomList } from "@/components/room/RoomList";
+import { redirect } from "next/navigation";
+import { Show } from "@/components/controlFlow/Show/Show";
+import Icon from "@/components/Icon";
+import Link from "next/link";
 
 const MyRooms = async ({ searchParams }: SearchParams) => {
   const currentUser = (await supabaseServerClient().auth.getSession()).data
     .session?.user;
-  invariant(currentUser);
+  if (!currentUser) redirect("/rooms");
+
   let roomsQuery = supabaseServerClient()
     .from("room")
     .select()
@@ -29,9 +33,7 @@ const MyRooms = async ({ searchParams }: SearchParams) => {
     return <ErrorMessage />;
   }
 
-  const fallback = (
-    <div className="text-gray-500">{$t("You have not added any rooms")}</div>
-  );
+  const canAddRooms = rooms.length <= 5;
 
   return (
     <div className="w-5/6 flex flex-col items-center gap-4">
@@ -41,11 +43,17 @@ const MyRooms = async ({ searchParams }: SearchParams) => {
       <div className="w-1/2">
         <RoomFilters className="w-full" />
       </div>
-      <RoomList
-        roomHref={(id) => `/rooms/${id}/admin`}
-        rooms={rooms}
-        fallback={fallback}
-      />
+      <RoomList roomHref={(id) => `/rooms/${id}/admin`} rooms={rooms}>
+        <Show when={canAddRooms}>
+          <Link
+            className="size-full flex flex-col justify-center items-center text-gray-500 hover:text-amber-500"
+            href="/rooms/add"
+          >
+            <Icon name="add" size="2xl" />
+            <span>{$t("Add room")}</span>
+          </Link>
+        </Show>
+      </RoomList>
     </div>
   );
 };
