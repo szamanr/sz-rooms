c461ab90950c04f3a300433eb39278fa4b2ed5f6
szaman <2622838+szamanr@users.noreply.github.com>
2024-03-11 12:54:57 +0000

    include supabase migrations in the project (#20)
    suppress hydration warnings in dev
    
    error from browser extension
    
    init supabase
    
    dump remote schema into migration

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
   modal: React.ReactNode;
 }) {
+  const isDev = process.env.NODE_ENV === "development";
+
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={isDev}>
       <body className={twClass(inter.className, "")}>
         <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-8 lg:p-24">
           {children}
